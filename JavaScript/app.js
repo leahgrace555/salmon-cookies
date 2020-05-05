824c05c8a1f448df3476090a152e4d8c551843e3
Leah Russo <63461171+leahgrace555@users.noreply.github.com>
2020-05-05 15:33:17 -0400

    render content to sales page

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -43,11 +43,6 @@ console.log(seattle.cookiesPerDay());
 
 //var outputSeattle = [`6am: ${seattle.cookiesperhourtaketwo()} cookies`,`7am: ${seattle.cookiesperhourtaketwo()} cookies`, `8am: ${seattle.cookiesperhourtaketwo()} cookies`, `9am: ${seattle.cookiesperhourtaketwo()} cookies`, `10am: ${seattle.cookiesperhourtaketwo()} cookies`, `11am: ${seattle.cookiesperhourtaketwo()} cookies`, `12pm: ${seattle.cookiesperhourtaketwo()} cookies`, `1pm: ${seattle.cookiesperhourtaketwo()} cookies`, `2pm: ${seattle.cookiesperhourtaketwo()} cookies`, `3pm: ${seattle.cookiesperhourtaketwo()} cookies`, `4pm: ${seattle.cookiesperhourtaketwo()} cookies`, `5pm: ${seattle.cookiesperhourtaketwo()} cookies`, `6pm: ${seattle.cookiesperhourtaketwo()} cookies`, `7pm: ${seattle.cookiesperhourtaketwo()} cookies`] //array of the times and randomly generated cookie sales
 
-//var content = document.getElementById('seattle'); //selecting the dom
-
-//var listItem = document.createElement('li'); //attempting to create list items
-//listItem.textContent = outputSeattle //attempting to write to the document but failing apparently
-
 
 //console.log(outputSeattle)
 
@@ -73,14 +68,33 @@ var tokyo = {
     var cookiesPerHourTokyo = this.randomCustomerNumber(this.minCustomer,this.maxCustomer) * this.avgCookiePerSale;
     //console.log(cookiesPerHourTokyo);
     return cookiesPerHourTokyo;
+  },
+  cookiesPerDay: function(){
+    var cookieHour = []
+    var sum = 0
+    for(var i=0;i<14;i++){
+      cookieHour.push(this.cookiesPerHourTokyo());
+      sum += this.cookiesPerHourTokyo();
+    }
+    cookieHour.push(sum);
+    return cookieHour;
+  },
+  render: function(){
+    var parent = document.getElementById('tokyo');
+    var array = this.cookiesPerDay();
+    var hour = ['6am: ','7am: ','8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ','2pm: ','3pm: ','4pm: ', '5pm: ','6pm: ','7pm: ', 'Total: '];
+    for(var i = 0;i<15;i++){
+      var listItem = document.createElement('li');
+      listItem.textContent = hour[i] + array[i] + ' cookies';
+      parent.appendChild(listItem);
+    }
   }
 };
 
-var outputTokyo = [`6am: ${tokyo.cookiesPerHourTokyo()} cookies`, `7am: ${tokyo.cookiesPerHourTokyo()} cookies`, `8am: ${tokyo.cookiesPerHourTokyo()} cookies`, `9am: ${tokyo.cookiesPerHourTokyo()} cookies`, `10am: ${tokyo.cookiesPerHourTokyo()} cookies`, `11am: ${tokyo.cookiesPerHourTokyo()} cookies`, `12pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `1pm: ${tokyo.cookiesPerHourTokyo()} cookies`,`2pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `3pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `4pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `5pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `6pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `7pm: ${tokyo.cookiesPerHourTokyo()} cookies` ];
-
-console.log(outputTokyo)
+//var outputTokyo = [`6am: ${tokyo.cookiesPerHourTokyo()} cookies`, `7am: ${tokyo.cookiesPerHourTokyo()} cookies`, `8am: ${tokyo.cookiesPerHourTokyo()} cookies`, `9am: ${tokyo.cookiesPerHourTokyo()} cookies`, `10am: ${tokyo.cookiesPerHourTokyo()} cookies`, `11am: ${tokyo.cookiesPerHourTokyo()} cookies`, `12pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `1pm: ${tokyo.cookiesPerHourTokyo()} cookies`,`2pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `3pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `4pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `5pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `6pm: ${tokyo.cookiesPerHourTokyo()} cookies`, `7pm: ${tokyo.cookiesPerHourTokyo()} cookies` ]
 
 tokyo.cookiesPerHourTokyo()
+tokyo.render()
 
 var dubai = {
   minCustomer: 11 ,
@@ -95,12 +109,33 @@ var dubai = {
     var cookiesPerHourDubai = this.randomCustomerNumber(this.minCustomer,this.maxCustomer) * this.avgCookiePerSale;
     //console.log(cookiesPerHourDubai);
     return cookiesPerHourDubai;
+  },
+  cookiesPerDay: function(){
+    var cookieHour = []
+    var sum = 0
+    for(var i=0;i<14;i++){
+      cookieHour.push(this.cookiesPerHourDubai());
+      sum += this.cookiesPerHourDubai();
+    }
+    cookieHour.push(sum);
+    return cookieHour;
+  },
+  render: function(){
+    var parent = document.getElementById('dubai');
+    var array = this.cookiesPerDay();
+    var hour = ['6am: ','7am: ','8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ','2pm: ','3pm: ','4pm: ', '5pm: ','6pm: ','7pm: ', 'Total: '];
+    for(var i = 0;i<15;i++){
+      var listItem = document.createElement('li');
+      listItem.textContent = hour[i] + array[i] + ' cookies';
+      parent.appendChild(listItem);
+    }
   }
 };
 
-var outputDubai = [`6am: ${dubai.cookiesPerHourDubai()} cookies`, `7am: ${dubai.cookiesPerHourDubai()} cookies`, `8am: ${dubai.cookiesPerHourDubai()} cookies`, `9am: ${dubai.cookiesPerHourDubai()} cookies`, `10am: ${dubai.cookiesPerHourDubai()} cookies`, `11am: ${dubai.cookiesPerHourDubai()} cookies`, `12pm: ${dubai.cookiesPerHourDubai()} cookies`, `1pm: `, `2pm: ${dubai.cookiesPerHourDubai()} cookies`, `3pm: ${dubai.cookiesPerHourDubai()} cookies`, `4pm: ${dubai.cookiesPerHourDubai()} cookies`, `5pm: ${dubai.cookiesPerHourDubai()} cookies`, `6pm: ${dubai.cookiesPerHourDubai()} cookies`, `7pm: ${dubai.cookiesPerHourDubai()} cookies`]
-console.log(outputDubai)
+//var outputDubai = [`6am: ${dubai.cookiesPerHourDubai()} cookies`, `7am: ${dubai.cookiesPerHourDubai()} cookies`, `8am: ${dubai.cookiesPerHourDubai()} cookies`, `9am: ${dubai.cookiesPerHourDubai()} cookies`, `10am: ${dubai.cookiesPerHourDubai()} cookies`, `11am: ${dubai.cookiesPerHourDubai()} cookies`, `12pm: ${dubai.cookiesPerHourDubai()} cookies`, `1pm: `, `2pm: ${dubai.cookiesPerHourDubai()} cookies`, `3pm: ${dubai.cookiesPerHourDubai()} cookies`, `4pm: ${dubai.cookiesPerHourDubai()} cookies`, `5pm: ${dubai.cookiesPerHourDubai()} cookies`, `6pm: ${dubai.cookiesPerHourDubai()} cookies`, `7pm: ${dubai.cookiesPerHourDubai()} cookies`]
+//console.log(outputDubai)
 dubai.cookiesPerHourDubai()
+dubai.render()
 
 var paris = {
   minCustomer: 20 ,
@@ -115,12 +150,33 @@ var paris = {
     var cookiesPerHourParis = this.randomCustomerNumber(this.minCustomer,this.maxCustomer) * this.avgCookiePerSale;
     //console.log(cookiesPerHourParis);
     return cookiesPerHourParis;
+  },
+  cookiesPerDay: function(){
+    var cookieHour = []
+    var sum = 0
+    for(var i=0;i<14;i++){
+      cookieHour.push(this.cookiesPerHourParis());
+      sum += this.cookiesPerHourParis();
+    }
+    cookieHour.push(sum);
+    return cookieHour;
+  },
+  render: function(){
+    var parent = document.getElementById('paris');
+    var array = this.cookiesPerDay();
+    var hour = ['6am: ','7am: ','8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ','2pm: ','3pm: ','4pm: ', '5pm: ','6pm: ','7pm: ', 'Total: '];
+    for(var i = 0;i<15;i++){
+      var listItem = document.createElement('li');
+      listItem.textContent = hour[i] + array[i] + ' cookies';
+      parent.appendChild(listItem);
+    }
   }
 };
 
-var outputParis = [`6am: ${paris.cookiesPerHourParis()} cookies`, `7am: ${paris.cookiesPerHourParis()} cookies`, `8am: ${paris.cookiesPerHourParis()} cookies`, `9am: ${paris.cookiesPerHourParis()} cookies`, `10am: ${paris.cookiesPerHourParis()} cookies`, `11am: ${paris.cookiesPerHourParis()} cookies`, `12pm: ${paris.cookiesPerHourParis()} cookies`, `1pm: ${paris.cookiesPerHourParis()} cookies`, `2pm: ${paris.cookiesPerHourParis()} cookies`, `3pm: ${paris.cookiesPerHourParis()} cookies`, `4pm: ${paris.cookiesPerHourParis()} cookies`, `5pm: ${paris.cookiesPerHourParis()} cookies`, `6pm: ${paris.cookiesPerHourParis()} cookies`,'7pm: ${paris.cookiesPerHourParis()} cookies']
-console.log(outputParis)
+//var outputParis = [`6am: ${paris.cookiesPerHourParis()} cookies`, `7am: ${paris.cookiesPerHourParis()} cookies`, `8am: ${paris.cookiesPerHourParis()} cookies`, `9am: ${paris.cookiesPerHourParis()} cookies`, `10am: ${paris.cookiesPerHourParis()} cookies`, `11am: ${paris.cookiesPerHourParis()} cookies`, `12pm: ${paris.cookiesPerHourParis()} cookies`, `1pm: ${paris.cookiesPerHourParis()} cookies`, `2pm: ${paris.cookiesPerHourParis()} cookies`, `3pm: ${paris.cookiesPerHourParis()} cookies`, `4pm: ${paris.cookiesPerHourParis()} cookies`, `5pm: ${paris.cookiesPerHourParis()} cookies`, `6pm: ${paris.cookiesPerHourParis()} cookies`,'7pm: ${paris.cookiesPerHourParis()} cookies']
+//console.log(outputParis)
 paris.cookiesPerHourParis()
+paris.render()
 
 var lima = {
   minCustomer: 2 ,
@@ -135,14 +191,34 @@ var lima = {
     var cookiesPerHourLima = this.randomCustomerNumber(this.minCustomer,this.maxCustomer) * this.avgCookiePerSale;
     //console.log(cookiesPerHourLima);
     return cookiesPerHourLima;
+  },
+  cookiesPerDay: function(){
+    var cookieHour = []
+    var sum = 0
+    for(var i=0;i<14;i++){
+      cookieHour.push(this.cookiesPerHourLima());
+      sum += this.cookiesPerHourLima();
+    }
+    cookieHour.push(sum);
+    return cookieHour;
+  },
+  render: function(){
+    var parent = document.getElementById('lima');
+    var array = this.cookiesPerDay();
+    var hour = ['6am: ','7am: ','8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ','2pm: ','3pm: ','4pm: ', '5pm: ','6pm: ','7pm: ', 'Total: '];
+    for(var i = 0;i<15;i++){
+      var listItem = document.createElement('li');
+      listItem.textContent = hour[i] + array[i] + ' cookies';
+      parent.appendChild(listItem);
+    }
   }
 
 };
 
-var outputLima = [`6am: ${lima.cookiesPerHourLima()} cookies`, `7am: ${lima.cookiesPerHourLima()} cookies`, `8am: ${lima.cookiesPerHourLima()} cookies`,`9am: ${lima.cookiesPerHourLima()} cookies`,`10am: ${lima.cookiesPerHourLima()} cookies`,`11am: ${lima.cookiesPerHourLima()} cookies`, `12am: ${lima.cookiesPerHourLima()} cookies`, `1pm: ${lima.cookiesPerHourLima()} cookies`,`2pm: ${lima.cookiesPerHourLima()} cookies`, `3pm: ${lima.cookiesPerHourLima()} cookies`, `4pm: ${lima.cookiesPerHourLima()} cookies`, `5pm: ${lima.cookiesPerHourLima()} cookies`, `6pm: ${lima.cookiesPerHourLima()} cookies`, `7pm: ${lima.cookiesPerHourLima()} cookies`]
-console.log(outputLima)
+//var outputLima = [`6am: ${lima.cookiesPerHourLima()} cookies`, `7am: ${lima.cookiesPerHourLima()} cookies`, `8am: ${lima.cookiesPerHourLima()} cookies`,`9am: ${lima.cookiesPerHourLima()} cookies`,`10am: ${lima.cookiesPerHourLima()} cookies`,`11am: ${lima.cookiesPerHourLima()} cookies`, `12am: ${lima.cookiesPerHourLima()} cookies`, `1pm: ${lima.cookiesPerHourLima()} cookies`,`2pm: ${lima.cookiesPerHourLima()} cookies`, `3pm: ${lima.cookiesPerHourLima()} cookies`, `4pm: ${lima.cookiesPerHourLima()} cookies`, `5pm: ${lima.cookiesPerHourLima()} cookies`, `6pm: ${lima.cookiesPerHourLima()} cookies`, `7pm: ${lima.cookiesPerHourLima()} cookies`]
+//console.log(outputLima)
 lima.cookiesPerHourLima()
-
+lima.render()
 console.log('hi')
 
 
